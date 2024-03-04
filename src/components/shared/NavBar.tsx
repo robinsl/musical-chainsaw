a2acb9138534b57ad0ebf41692c79d42506bab14
Robin <robinlamarre@gmail.com>
2024-03-03 23:30:18 -0500

    add alt

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -7,8 +7,8 @@ import {
 } from "@/components/ui/navigation-menu";
 import Link from "next/link";
 import {usePathname} from "next/navigation";
-import {Button} from "@/components/ui/button";
 import {StyleToggle} from "@/components/shared/StyleToggle";
+import Image from "next/image";
 
 export default function NavBar() {
   const pathname = usePathname()
@@ -16,7 +16,7 @@ export default function NavBar() {
   return <>
     <div className={"border-b"}>
       <div className={"flex h-16 px-4 items-center"}>
-        <img src={"https://help.kagi.com/assets/doggo_1.b29ed36f.png"} className={"inline-flex h-16"}/>
+        <Image src={"https://help.kagi.com/assets/doggo_1.b29ed36f.png"} className={"inline-flex h-16"} alt={"kagi logo"} />
         <NavigationMenu className={"px-4"}>
           <NavigationMenuList className={"space-x-4 lg:space-x-6"}>
             <NavigationMenuItem className={pathname === "/" ? "" : "text-muted-foreground"}>
